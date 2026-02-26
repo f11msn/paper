a3f09f564a37723eb6b5352ab8637146f5521040
Valentin Shevlyakov <valentin.shevlyakov@gmail.com>
2026-02-26 09:46:54 +0300

    Strip XML artifacts from model output, clean article previews
    DeepSeek at high temperature can hallucinate XML-like tool call
    markup instead of proper JSON tool_calls. Strip these before rendering.
    Also use strip_markdown for article index previews.

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,6 +1,14 @@
 module MarkdownHelper
+  ALLOWED_TAGS = %w[
+    p br h1 h2 h3 h4 h5 h6 strong em a ul ol li
+    code pre blockquote table thead tbody tr th td
+    hr del img span div
+  ].freeze
+
   def render_markdown(text)
     return "" if text.blank?
+
+    cleaned = strip_xml_artifacts(text)
 
     markdown = Redcarpet::Markdown.new(
       Redcarpet::Render::HTML.new(hard_wrap: true),
@@ -11,10 +19,22 @@ module MarkdownHelper
       no_intra_emphasis: true
     )
 
-    sanitize(markdown.render(text), tags: %w[
-      p br h1 h2 h3 h4 h5 h6 strong em a ul ol li
-      code pre blockquote table thead tbody tr th td
-      hr del img span div
-    ], attributes: %w[href src alt class title])
+    sanitize(markdown.render(cleaned), tags: ALLOWED_TAGS, attributes: %w[href src alt class title])
+  end
+
+  def strip_markdown(text)
+    return "" if text.blank?
+
+    strip_tags(render_markdown(text)).squish
+  end
+
+  private
+
+  def strip_xml_artifacts(text)
+    text
+      .gsub(%r{</?function_calls>}i, "")
+      .gsub(%r{</?invoke[^>]*>}i, "")
+      .gsub(%r{</?parameter[^>]*>}i, "")
+      .gsub(%r{</?antml:[^>]*>}i, "")
   end
 end
